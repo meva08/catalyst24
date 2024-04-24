526655dd36dff8918facc69c82a5d548de212a61
Maryeva Gonzalez <144571162+meva08@users.noreply.github.com>
2024-04-24 09:43:06 -0400

    Inputting a navigation bar linked to all of the pages (#5)

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -3,19 +3,22 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import MyRecipes from "./pages/MyRecipes";
 import {Routes, Route} from "react-router-dom";
-
+import Navbar from "./pages/Navbar";
 import './App.css';
 
 function App() {
   return (
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="my_recipes" element={<MyRecipes />} />
-          </Route>
-        </Routes>
+    <><Navbar/>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="my_recipes" element={<MyRecipes />} />
+      </Route>
+    </Routes></>
+        
   );
+  
 }
 
 export default App;
